799c0a378cd4796adde158dcf8ac193b4e95cd1c
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2018-09-05 16:49:40 +1200

    Create pseudocode_level1.py

---
+++ smallprojects/pseudocode_level1.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python2
+##WRITTEN BY LUDOVIC DUTOIT, AUGUST 2018
+#AIM: This scrpit aims at detecting windows that are suitable for amplification in a Fasta alignment
+#It is full of conditions and the proposed primers should always checked visually in the alignbment using a simple alignment visualisation software
+
+
+#Read the multi fasta file
+
+
+
+#for window by window from the length of the primer, go through the alignment shifting 1 base at the time
+
+  # store primer sequences
+  # store window sequences
+  #calculate diversity
+  # check for invariable sites at the end of primer
+  # Look at the above and our input and see if the window is good or not
+
+
+Output the okay window to a file
